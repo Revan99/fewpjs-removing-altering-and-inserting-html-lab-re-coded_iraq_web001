0bc97926e8bbde972f24f94e43280e8c1365c6e7
Revan Sarbast <revansarbast8@gmail.com>
2020-05-15 17:52:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 let main = document.querySelector('main#main');
 main.remove();
-let header = document.createElement('h1');
-document.body.appendChild(header);
-header.className = 'victory';
 let newHeader = document.querySelector('h1#victory');
 newHeader.innerHTML = 'Revan is the champion';
-console.log(header);
