46e31b631b0e0626a27e3d4091eab21aadfc1599
Revan Sarbast <revansarbast8@gmail.com>
2020-05-15 17:42:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 let main = document.querySelector('main#main');
 main.remove();
-
-let newHeader = document.createElement('h1');
-newHeader.className = "victory";
-newHeader.innerHTML = 'YOUR-NAME is the champoin'
+let header = document.createElement('h1');
+document.body.append()
